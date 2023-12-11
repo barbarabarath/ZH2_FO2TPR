5a2873068ef81b5ffaf4583712a0765fca4389b8
Barbara Vanda Baráth <barath.barbara.vanda@gmail.com>
2023-12-11 14:59:43 +0100

    Gomb színezés

--- ZH2_FO2TPR/Form2.Designer.cs
+++ ZH2_FO2TPR/Form2.Designer.cs
@@ -44,42 +44,46 @@
             // 
             // button1
             // 
+            button1.BackColor = Color.FromArgb(192, 192, 255);
             button1.Location = new Point(12, 462);
             button1.Name = "button1";
             button1.Size = new Size(200, 23);
             button1.TabIndex = 1;
             button1.Text = "Szálláshelyek összehasonlítása";
-            button1.UseVisualStyleBackColor = true;
+            button1.UseVisualStyleBackColor = false;
             button1.Click += button1_Click;
             // 
             // button2
             // 
+            button2.BackColor = Color.FromArgb(255, 192, 255);
             button2.Location = new Point(293, 462);
             button2.Name = "button2";
             button2.Size = new Size(132, 23);
             button2.TabIndex = 2;
             button2.Text = "Foglalás rögzítése";
-            button2.UseVisualStyleBackColor = true;
+            button2.UseVisualStyleBackColor = false;
             button2.Click += button2_Click;
             // 
             // button3
             // 
+            button3.BackColor = Color.FromArgb(255, 192, 192);
             button3.Location = new Point(507, 462);
             button3.Name = "button3";
             button3.Size = new Size(149, 23);
             button3.TabIndex = 3;
             button3.Text = "Foglalás módosítás";
-            button3.UseVisualStyleBackColor = true;
+            button3.UseVisualStyleBackColor = false;
             button3.Click += button3_Click;
             // 
             // button4
             // 
+            button4.BackColor = Color.FromArgb(255, 224, 192);
             button4.Location = new Point(762, 462);
             button4.Name = "button4";
             button4.Size = new Size(51, 23);
             button4.TabIndex = 4;
             button4.Text = "+";
-            button4.UseVisualStyleBackColor = true;
+            button4.UseVisualStyleBackColor = false;
             button4.Click += button4_Click;
             // 
             // Form2
